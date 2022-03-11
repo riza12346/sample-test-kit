8dc4acfd9445eee158d283430933741ad7136072
Mac Jovel Sanchez <*joysanchez817@gmail.com*>
2022-03-11 18:01:54 +0800

    activyty3

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import { Dimensions, Image, StyleSheet } from 'react-native';
-
+import { TextInput } from 'react-native-paper';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Button } from "react-native-elements";
+import LottieView from 'lottie-react-native'
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) { 
+  const[email, setEmail]=useState<string>("");
+  const[password, setPassword]=useState<string>("");
   return (
     <View style={{
-      flex: 1,
+      flex: 0,
       justifyContent: 'center',
       alignItems: 'center'
     }}>
       <View style={{
-        height: '75%',
-        width: '80%',
+        height: '100%',
+        width: '100%',
         backgroundColor: '#E7EEF1',
         borderRadius: 10,
         overflow: 'hidden',
@@ -29,37 +34,79 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         flexDirection: 'column'
       }}>
 
-        <View style={{ flex: 2, borderRadius: 10, overflow: 'hidden' }}>
-          <Image
-            source={{ uri: 'https://manofmany.com/wp-content/uploads/2021/09/What-is-an-NFT-1.jpg' }}
-            style={{
-              height: '100%',
-              width: '100%',
-              resizeMode: 'cover',
-            }}
-          />
-        </View>
-
-        <View style={{ flex: 0, borderRadius: 10, marginVertical: 10, padding: 10 }}>
-          <Text style={{
-            fontSize: 25,
-            color: '#525454',
-            fontFamily: 'poppins-semi-bold'
-          }}>Title Here</Text>
-        </View>
-
-        <View style={{ flex: 0, padding: 10, borderRadius: 10 }}>
-          <Text style={{
-            fontSize: 18,
-            color: '#999B9B',
-            fontFamily: 'poppins-regular',
-          }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s,
-          </Text>
-        </View>
-
+<View style={{
+  flex:2,
+  backgroundColor:'white' }}> 
+  <LottieView
+  source={require('../../../New folder/sample-test-kit/assets/lottie/53395-login.json')}
+autoPlay={true}
+loop={true}
+/>
       </View>
+
+        <View style={{
+          flex: 0,
+          justifyContent: 'center',
+          paddingHorizontal: 20,
+          backgroundColor: 'white'
+        }}>
+
+        <TextInput
+        
+        label="Email"
+        value={email}
+        autoComplete={false}
+        style={{marginBottom: 10}}
+        onChangeText={(text: string) => {
+          setEmail(text);
+        }}
+        />
+       <TextInput
+        label="Password"
+        secureTextEntry={true}
+        value={password}
+        autoComplete={false}
+        style={{marginBottom: 10}}
+        onChangeText={(text: string) => {
+          setEmail(text);
+        }}
+        />
+
+      </View> 
+      <View style={{
+        flex: 0, 
+        width: '100%', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor:'white'
+      }}>
+        
+      <Button
+      title={'LOGIN'}
+      buttonStyle={{
+      backgroundColor: '#7209b7',
+      borderRadius: 10,
+      padding: 20,
+      paddingHorizontal: 40,
+      marginBottom: 10,
+      }}
+  
+/>
+<Button
+      title={'Sign up'}
+      buttonStyle={{
+      backgroundColor: '#7209b7',
+      borderRadius: 10,
+      padding: 20,
+      paddingHorizontal: 40,
+      marginBottom: 10,
+      }}
+      />
     </View>
+    </View>
+    </View>
+   
   );
 }
+
+
